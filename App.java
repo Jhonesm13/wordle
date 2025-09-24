dc2dbed2d128c59f686fd0b3e7a981eae86426cb
Jhonesm13 <jhone.menezes@edu.pucrs.br>
2025-09-24 05:48:03 -0300

    implementado novas validações para ver se a palavra é válida.

--- App.java
+++ App.java
@@ -1,16 +1,17 @@
+import java.text.Normalizer;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String palavra = getPalavraAleatoria().toLowerCase();
+    private static final String palavraLimpa = limpaInput(palavra).toLowerCase();
 
     public static void main(String[] args) {
-        String palavra = getPalavraAleatoria();
-
-        System.out.println("Palavra sorteada: " + palavra);
+        System.out.println("Palavra sorteada: " + palavraLimpa);
 
         for(int i=0;i<6;i++){
-            Boolean result = descobrePalavra(palavra);
+            Boolean result = descobrePalavra();
             if(result) {
                 System.out.println("Você acertou a palavra: "+palavra);
                 break;
@@ -18,19 +19,52 @@ public class App {
                 System.out.println("Errouuuu");
             }    
         }
-        
         scanner.close();
-
     }
 
-    public static Boolean descobrePalavra(String palavra) {
-        System.out.println("Digite a palavra:");
-        String palavraEscolhida = scanner.nextLine();
-        if(palavraEscolhida.equals(palavra)) {
+    public static Boolean descobrePalavra() {
+        boolean palavraValida = false;
+        String palavraEscolhida;
+
+        do{
+            System.out.println("Digite a palavra:");
+            palavraEscolhida = scanner.nextLine();
+            palavraEscolhida = limpaInput(palavraEscolhida);
+
+            if(palavraEscolhida.length() != 5) {
+               System.out.println("Digite uma palavra de 5 letras.");
+                continue; 
+            }
+            
+            if (!existeNoDicionario(palavraEscolhida)) {
+                System.out.println("Essa palavra não é válida.");
+                continue;
+            }
+
+            palavraValida = true;
+        } while(!palavraValida);
+
+        if(palavraEscolhida.equals(palavraLimpa)) {
             return true;
         }else {
             return false;
         }
+    }
+
+    public static boolean existeNoDicionario(String palavra) {        
+        for (String palavraDicionario : WordleDictionary.WORDS) {
+            String palavraDicionarioLimpa = limpaInput(palavraDicionario);
+            
+            if (palavraDicionarioLimpa.equals(palavraLimpa)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String limpaInput(String palavra) {
+        String palavraLimpa = Normalizer.normalize(palavra, Normalizer.Form.NFD).trim();
+        return palavraLimpa.replaceAll("\\p{InCombiningDiacriticalMarks}+", "").toLowerCase();
     }
 
     public static String getPalavraAleatoria() {
